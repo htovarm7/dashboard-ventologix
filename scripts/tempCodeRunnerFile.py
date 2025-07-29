e6034883f936a5026f5076f9288a54796aa651f6
Hector <h.tovarm07@gmail.com>
2025-07-29 11:52:58 -0600

    refactor: remove unused temporary scripts to clean up the repository

--- scripts/tempCodeRunnerFile.py
+++
@@ -1,9 +0,0 @@
-    try:
-        with smtplib.SMTP(smtp_server, smtp_port) as smtp:
-            smtp.set_debuglevel(1)  # 🔍 VER MÁS DETALLES
-            smtp.starttls()
-            smtp.login(from_address, smtp_password)
-            smtp.send_message(msg)
-        print(f"Correo enviado con {os.path.basename(pdf_file_path)}")
-    except Exception as e:
-        print(f"❌ Error al enviar el correo: {e}")
