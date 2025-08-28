8c6b7089d5b31be9bd69da9a0a145a1008e66c9f
Hector <h.tovarm07@gmail.com>
2025-08-28 10:06:42 -0600

    fix: correct database variable name in checkDataBase.py and ensure log message consistency in sendMail.sh

--- scripts/VM/checkDataBase.py
+++ scripts/VM/checkDataBase.py
@@ -15,7 +15,7 @@ load_dotenv()
 DB_HOST = os.getenv("DB_HOST")
 DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
-DB_NAME = os.getenv("DB_NAME")
+DB_DATABASE = os.getenv("DB_DATABASE")
 
 alias_name = "VTO LOGIX"
 smtp_from = "andres.mirazo@ventologix.com"
@@ -62,7 +62,7 @@ def check_data():
             host=DB_HOST,
             user=DB_USER,
             password=DB_PASSWORD,
-            database=DB_NAME
+            database=DB_DATABASE
         )
         cursor = conn.cursor()
 
