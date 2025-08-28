e651c94307cf3ca56422bc965302ca879df665af
Hector <h.tovarm07@gmail.com>
2025-08-28 10:08:31 -0600

    fix: correct database variable name from DB_NAME to DB_DATABASE in acrel.py and mqtt_to_mysql.py

--- scripts/VM/acrel.py
+++ scripts/VM/acrel.py
@@ -18,7 +18,7 @@ MQTT_TOPIC = "ADW300/TEST1"
 DB_HOST = os.getenv("DB_HOST")
 DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
-DB_NAME = os.getenv("DB_NAME")
+DB_DATABASE = os.getenv("DB_DATABASE")
 
 def find_val(points, id):
     return next((p["val"] for p in points if p["id"] == id), 0)
@@ -43,7 +43,7 @@ def insert_data(payload):
             host=DB_HOST,
             user=DB_USER,
             password=DB_PASSWORD,
-            database=DB_NAME
+            database=DB_DATABASE
         )
 
         if connection.is_connected():
--- scripts/VM/mqtt_to_mysql.py
+++ scripts/VM/mqtt_to_mysql.py
@@ -19,7 +19,7 @@ db_config = {
     "host": os.getenv("DB_HOST"),
     "user": os.getenv("DB_USER"),
     "password": os.getenv("DB_PASSWORD"),
-    "database": os.getenv("DB_NAME")
+    "database": os.getenv("DB_DATABASE")
 }
 
 # Configurar logging
