32c0607b9b7531178c686036610c8b61e55f0c33
Hector <h.tovarm07@gmail.com>
2025-09-04 09:44:10 -0600

    refactor: clean up api_server.py by removing unnecessary comments and organizing CORS origins

--- scripts/api_server.py
+++ scripts/api_server.py
@@ -1,37 +1,5 @@
-"""
- * @file fastApi.py
- * @date 23/04/2025
- * @author Hector Tovar
- * 
- * @description
- * This file implements fetching data from the database using a FastAPI server and MySQL connector.
- * Based on the data fetched, it returns the data, which is then sent to the graphs.
- * @version 1.0
-
-"""
-
-"""
-* @Observations:
-* 1. To run the API, use the command:
-* uvicorn scripts.api_server:app --reload
-* To check the API response, you can use the following URL:
-* http://127.0.0.1:8000/docs
-* For PENOX use device_id = 7
-* If the API is not updating, check the following:
-* 1. Run in terminal:
-    tasklist | findstr python
-* 2. If the process is running, kill it using:
-    taskkill /F /PID <PID>
-* 3. Where <PID> is the process ID obtained from the previous command, which in this case is 18168.
-    python.exe                   18168 Console                    1    67,276 KB
-* 4. Run the API again using:
-"""
-
-from fastapi import FastAPI, Depends, HTTPException, status, Header
+from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from typing import Annotated
-
-
 import os
 from dotenv import load_dotenv
 
@@ -42,14 +10,20 @@ from scripts.api.webApi import web
 load_dotenv()
 
 app = FastAPI()
-    
+
+origins = [
+    "https://dashboard.ventologix.com",  
+    "http://localhost",                 
+    "http://localhost:3000",             
+    "http://127.0.0.1:8000",            
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-    expose_headers=["*"],
+    allow_methods=["GET", "POST", "PUT", "DELETE"],
+    allow_headers=["*"], 
 )
 
 # Incluir routers
