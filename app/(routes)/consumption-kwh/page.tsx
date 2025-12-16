b830eca3f309a58a844e56a38016d81162e57833
Héctor Tovar <h.tovarm07@gmail.com>
2025-12-16 14:10:26 -0600

    fix

--- app/(routes)/consumption-kwh/page.tsx
+++ app/(routes)/consumption-kwh/page.tsx
@@ -4,7 +4,7 @@ import React, { useEffect, useState } from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 import { useRouter } from "next/navigation";
 import BackButton from "@/components/BackButton";
-import { Line, Bar } from "react-chartjs-2";
+import { Line } from "react-chartjs-2";
 import {
     Chart as ChartJS,
     CategoryScale,
